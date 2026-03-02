c70d21649a85da4c6f15e0dea8e5d88b830c81e5
StrangelySafeNova <ericfischery@gmail.com>
2026-03-02 11:34:58 -0500

    Add weapon framework

--- scripts/components/weapon.gd
+++ scripts/components/weapon.gd
@@ -3,8 +3,18 @@ extends Node2D
 
 @onready var _collision_shape = $CollisionShape2D
 
+# nom de l'animation donné lors de la création de l'animation
 @export var attack_animation: String
+# longueur de l'animation (en secondes)
+@export var attack_length: float
+# animation player qui contient l'animation
+@export var animation_player: AnimationPlayer
 
+func attack() -> void:
+	animation_player.play(attack_animation)
+
+# Pour les attaques, l'animation devrait être utilisée pour activer/désactiver
+# les hitboxes, pas les fonctions
 func disable() -> void:
 	_collision_shape.set_disable(true)
 
