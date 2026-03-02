cb6482af629e85807cd4e35278585391ff63dced
StrangelySafeNova <ericfischery@gmail.com>
2026-03-01 20:58:05 -0500

    Add player walk animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 
 func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -12,12 +14,19 @@ func _physics_process(_delta: float) -> void:
 	var ydirection := Input.get_axis("ui_up", "ui_down")
 	var direction: Vector2 = Vector2(xdirection, ydirection).normalized()
 
-	if direction:
+	if direction.x:
 		velocity.x = direction.x * SPEED
 		velocity.y = direction.y * SPEED
+		animated_sprite.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		animated_sprite.play("idle")
+	
+	if direction.x < 0:
+		animated_sprite.flip_h = true
+	elif direction.x > 0:
+		animated_sprite.flip_h = false
 		
 	velocity.normalized()
 
