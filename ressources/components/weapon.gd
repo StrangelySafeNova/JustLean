acd1a7f7b9791cd964cb03a2bb7ddb50f66c2232
StrangelySafeNova <ericfischery@gmail.com>
2026-03-03 18:43:37 -0500

    Add goblin state machine

--- ressources/components/weapon.gd
+++ ressources/components/weapon.gd
@@ -5,8 +5,6 @@
 class_name Weapon
 extends Node2D
 
-@onready var _collision_shape = $CollisionShape2D
-
 # nom de l'animation donné lors de la création de l'animation (dans meat)
 @export var attack_animation: String
 # longueur de l'animation (en secondes)
@@ -28,11 +26,3 @@ func _ready() -> void:
 
 func attack() -> void:
 	meat.play(attack_animation)
-
-# Pour les attaques, l'animation devrait être utilisée pour activer/désactiver
-# les hitboxes, pas les fonctions
-func disable() -> void:
-	_collision_shape.set_disable(true)
-
-func enable() -> void:
-	_collision_shape.set_disable(false)
