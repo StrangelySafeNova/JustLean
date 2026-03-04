529d0e3fa91930f29e09dfaacf7e41a8cc45cb69
StrangelySafeNova <ericfischery@gmail.com>
2026-03-03 20:18:28 -0500

    Fix goblin animation & y sort

--- ressources/state_machine/states/follow_state.gd
+++ ressources/state_machine/states/follow_state.gd
@@ -3,6 +3,7 @@ extends State
 
 @export var this: CharacterBody2D
 @export var speed: float
+@export var stop_distance: int = 10
 
 func enter() -> void:
 	pass
@@ -17,3 +18,10 @@ func physics_update(_delta: float) -> void:
 	if this && owner.player:
 		var direction = owner.player.position - this.position
 		owner.velocity = direction.normalized() * speed
+
+		if direction.length() <= stop_distance:
+			owner.velocity = Vector2.ZERO
+
+			# joue l'animation idle seulement si elle ne joue pas déjà
+			if owner.animated_sprite.animation != "idle":
+				owner.animated_sprite.play("idle")
